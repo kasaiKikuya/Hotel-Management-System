460d1e3fd12ee9596ef5662ef486df6e65ac7f65
Phong Vo <84026292+kasaiKikuya@users.noreply.github.com>
2024-02-17 17:29:04 +0700

    change

--- .history/db_20240113060657.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-//$connection = mysqli_connect("localhost","root","","hotelms");
-$connection = mysqli_connect("localhost","user","password","hotelms");
--- .history/leetcode_20240119152841.js
+++
@@ -1,9 +0,0 @@
-var addTwoNumbers = function (l1, l2) {
-  const iter = (n1, n2, extra = 0) => {
-    if (!n1 && !n2 && !extra) return null;
-    const newVal = (n1?.val || 0) + (n2?.val || 0) + extra;
-    const nextNode = iter(n1?.next, n2?.next, Math.floor(newVal / 10));
-    return new ListNode(newVal % 10, nextNode);
-  };
-  return iter(l1, l2);
-};
